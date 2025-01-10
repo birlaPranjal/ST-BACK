6e34efd0284fa15683937d58ee88063c7a1486b0
Pranjal birla <120776389+birlaPranjal@users.noreply.github.com>
2025-01-10 16:08:16 +0530

    added get route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.listen(PORT, () => {
     console.log(`server running at port : ${PORT}`);
 });
 
+app.get("/", (req, res) => {
+    res.send("Hello World the server is running");
+})
+
 // App error event handling
 app.on("error", (error) => {
     console.log("App error: ", error);
